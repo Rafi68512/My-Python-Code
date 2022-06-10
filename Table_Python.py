75860098ab0eb51095f3dd1a9fe220178d748e25
Muhammad Rafi <90509516+Rafi68512@users.noreply.github.com>
2022-06-10 09:27:44 +0700

    Add files via upload

---
+++ Table_Python.py
@@ -0,0 +1,40 @@
+"""
+Pemrogram: Muhammad Rafi
+Nama berkas: Table_Python.py
+Tujuan : Membuat Tabel
+Interpreter : Python 3.10.4
+Text Editor: Visual Code Studio
+Operating System: Windows 11
+"""
+
+print('Nama: Muhammad Rafi')
+print("="*20, "\n")
+
+# membuat tabel cara 1
+print('=' * 30, 'CARA 1', '=' * 30)
+from prettytable import PrettyTable
+
+x = PrettyTable()
+
+x.field_names = (["Nama", "Nim", "Mata Kuliah Favorit"])
+
+x.add_row(["Muhammad Rafi", "2130511003", "Sistem Digital" ])
+x.add_row(["Faisal A. F. Munawar", "2130511023", "Algoritma dan Struktur Data"])
+
+print(x)
+
+
+# membuat tabel cara 2
+print("\n")
+print('=' * 30, 'CARA 2', '=' * 30, )
+from prettytable import PrettyTable
+
+x = PrettyTable()
+
+column_names = ["Nama", "Nim", "Mata Kuliah Favorit"]
+
+x.add_column(column_names[0], ["Muhammad Rafi", "Faisal A. F. Munawar"])
+x.add_column(column_names[1], ["2130511003", "2130511023",])
+x.add_column(column_names[2], ["Sistem Digital", "Algoritma dan Struktur Data"])
+
+print(x)
